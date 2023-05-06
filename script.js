7b6aae0120c37b12a15c8501eda2c146ffc65a07
Tanmay Sharma <97392494+tookstanmay@users.noreply.github.com>
2023-05-06 10:47:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+async function getQuote(){
+
+    const response = await fetch("https://type.fit/api/quotes?_limit=100");
+
+    if (response.status == 404){
+        console.log("Bad Request!");
+    }
+    else{
+        var data = response.json();
+        
+        let quote = await data;
+        let index = randomValue(quote.length);
+        let text = quote[index].text;
+        let author = quote[index].author;
+        
+        if (author == "")
+            author = "Unknown";
+        document.querySelector(".quoteText").innerHTML= text;
+        document.querySelector(".quoteAuthor").innerHTML= author;
+    }
+}
+
+getQuote();
+
+function randomValue(length){
+    let value = Math.round((Math.random() * length));
+    console.log(value);
+    return value;
+}
+
+function randomBackground(){
+    document.body.style.background="url('https://source.unsplash.com/1920x1080?nature?dark')";
+}
